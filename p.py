295eed0dc30637f4ddd02add847018c35f65f487
mahongquan <mahongquan@sina.com>
2016-08-17 11:20:50 +0800

    val decl

--- p.py
+++ p.py
@@ -4,7 +4,32 @@ import clang.cindex
 from clang.cindex import Config
 Config.set_library_path(r"D:\LLVM\bin")
 index = clang.cindex.Index.create()
-tu = index.parse("p/src/main2.c")
+fn="p/src/global.c"
+f=open(fn,"r")
+fc=f.readlines()
+fc.append('')
+f.close()
+tu = index.parse(fn)
+def showSource(cursor):
+	#print(fc,len(fc))
+	start=[cursor.extent.start.line-1,cursor.extent.start.column-1]
+	end=[cursor.extent.end.line-1,cursor.extent.end.column-1]
+	#print(start,end)
+	fc1=fc[start[0]:]
+	end[0]=end[0]-start[0]
+	start[0]=0
+	fc1[0]=fc1[0][start[1]:]
+	# if cursor.kind==clang.cindex.CursorKind.FUNCTION_DECL:
+	# 	print(fc1)
+	# 	input("hi")
+	#print(start,end)
+	if end[0]==start[0]:
+		end[1]=end[1]-start[1]
+		fc1[0]=fc1[0][:end[1]+1]
+		return fc1[0]
+	else:
+		fc1[end[0]]=fc1[end[0]][:end[1]]
+		return fc1[0:end[0]+1]
 def showToken(node):
     r=[]
     ts=node.get_tokens()
@@ -51,7 +76,8 @@ def visit_FUNCTION_DECL(cursor):
 			r.append(",")
 		i+=1
 	r.append(")")
-	r.append(visit(cs[len(cs)-1]))#visit_COMPOUND_STMT(cs[len(cs)-1]))
+	if len(cs)-1>0:
+		r.append(visit(cs[len(cs)-1]))#visit_COMPOUND_STMT(cs[len(cs)-1]))
 	return r
 def visit_RETURN_STMT(cursor):
 	print("visit return")
@@ -69,32 +95,41 @@ def visit_INTEGER_LITERAL(cursor):
 		r.append(t.spelling)
 	return r#cursor.displayname#showToken(cursor)
 def visit_unknown(cursor):
-	r=[cursor.spelling]
+	r=[]
 	for c in cursor.get_children():
 		r.append(visit(c))
 	return r
 def visit_UNARY_OPERATOR(cursor):# a++;
 	r=[]
-	r.append(showToken(cursor))
-	r.append(":")
+	for t in cursor.get_tokens():
+		r.append(t.spelling)
+	return r#cursor.displayname#showToken(cursor)
+def visit_TRANSLATION_UNIT(cursor):
+	r=[]
 	for c in cursor.get_children():
 		r.append(visit(c))
-	return r	
+	return r
 def visit_DECL_REF_EXPR(cursor):#a++
 	r=[]
-	r.append(showToken(cursor))
-	r.append(":")
-	for c in cursor.get_children():
-		r.append(visit(c))
-	return r	
+	for t in cursor.get_tokens():
+		r.append(t.spelling)
+	return r#cursor.displayname#showToken(cursor)
+def visit_VAR_DECL(cursor):#a++
+	r=[]
+	for t in cursor.get_tokens():
+		r.append(t.spelling)
+	return r#cursor.displayname#showToken(cursor)
 def visit(cursor):
-	print("=============")
-	print(cursor.kind,cursor.displayname)
-	cs=[]
+	print("=cursor info===============================================")
+	print(cursor.kind)
+	print(showSource(cursor))
 	for c in cursor.get_children():
-		cs.append(str(c.kind))
-	print(",".join(cs))	
-	if cursor.kind==clang.cindex.CursorKind.FUNCTION_DECL:
+		print(c.kind,showSource(c))
+	print("=end cursor info============================================")
+	
+	if cursor.kind==clang.cindex.CursorKind.TRANSLATION_UNIT:
+		return visit_TRANSLATION_UNIT(cursor)
+	elif cursor.kind==clang.cindex.CursorKind.FUNCTION_DECL:
 		return visit_FUNCTION_DECL(cursor)
 	elif cursor.kind==clang.cindex.CursorKind.PARM_DECL:
 		return visit_PARM_DECL(cursor)
@@ -108,6 +143,8 @@ def visit(cursor):
 		return visit_UNARY_OPERATOR(cursor)
 	elif cursor.kind==clang.cindex.CursorKind.DECL_REF_EXPR:
 		return visit_DECL_REF_EXPR(cursor)
+	elif cursor.kind==clang.cindex.CursorKind.VAR_DECL:
+		return visit_VAR_DECL(cursor)
 	else:
 		return visit_unknown(cursor)
 def showResult(l):
@@ -123,7 +160,7 @@ def showResult(l):
 	return " ".join(r)
 print(dir(tu.cursor))	
 res=visit(tu.cursor)
-print(res)
+print("res",res)
 print(showResult(res))
 # print(dir(tu.cursor))
 # func1=None
